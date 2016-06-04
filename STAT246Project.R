885b4f59ba78ca0742ae62929933db96b20757b6
oconnor-kevin <oconnor.kevin.ant@gmail.com>
2016-06-04 00:28:36 -0500

    Completed!  Time to graduate.

--- STAT246Project.R
+++ STAT246Project.R
@@ -16,34 +16,22 @@ training.label <- rep(0:9, num.training) # Labels of training data
 test.label <- rep(0:9, num.test) # Labels of test data
 
 
-## Cross validation
-lambda = seq(0.05, 0.95, 0.05)
-p = rep(0.1, 10)
-errors = list()
-for(i in 1:length(lambda)){
-	error_i = c()
-	for (j in 1:5){
-		mu = list()
-		cv.train.data = list()
-		cv.test.data = list()
-		cv.test.labels = list()
-		for (k in 0:9){
-			inds = sample(500,400)
-			cv.train.data[[k+1]] = training.data[which(training.label==k),][inds,]+0
-			cv.test.data[[k+1]] = training.data[which(training.label==k),][-inds,]+0
-			cv.test.labels[[k+1]] = training.label[which(training.label==k)][-inds]
-			mu[[k+1]] = colMeans(cv.train.data[[k+1]])
-		}
-		sigma = cov(matrix(unlist(cv.train.data), nrow=4000, byrow=T))
-		cv.test.data = matrix(unlist(cv.test.data), nrow=1000, byrow=T)
-		cv.test.labels = unlist(cv.test.labels)		
-		error_i = c(error_i, test_classifier(mu, sigma, p, lambda[i], cv.test.data, cv.test.labels)[[1]])
+## Compute Sigma
+compute_sigma <- function(mu, labels, x){
+	total = (x[1,]-mu[[labels[1]+1]])%*%t(x[1,]-mu[[labels[1]+1]]) # initialize
+	for (i in 2:length(x[,1])){ # loop through samples
+		total = total + ((x[i,]-mu[[labels[i]+1]])%*%t(x[i,]-mu[[labels[i]+1]])) # add to sum
 	}
-	errors[[i]] = error_i
+	return((1/length(x[,1]))*total) # divided by n
 }
-pdf("ErrorPlot.pdf", width=13, height=8)
-plot(sort(rep(lambda,5)), unlist(errors), main="Error Rate vs. Lambda", xlab="Lambda", ylab="Error Rate")
-dev.off()
+
+
+## Covariance matrix regularization and inversion method.
+inv_reg_covmat <- function(sigma, lambda){
+	sigma_l = (1-lambda)*sigma + (lambda/4)*diag(length(sigma[1,])) # regularize as suggested
+	return(ginv(sigma_l)) # return inverse of regularized matrix
+}
+
 
 ## Bayes Classification Method
 ### Returns the predicted digit based on Bayes classification as specified in Part a.
@@ -68,46 +56,70 @@ test_classifier <- function(mu, sigma, p, lambda, x, labels){
 	for (i in 1:length(x[,1])){ # loop through examples in test set x
 		classes = c(classes, bayes_classify(mu, sig_inv, p, x[i,])) # append to vector of predicted classes
 	}
-	error_rate = mean((labels == classes)+0) # compute error rate
+	error_rate = 1-mean((labels == classes)+0) # compute error rate
 	return(list(error_rate, classes)) # return error rate and predicted classes
 }
 
 
-## Covariance matrix regularization and inversion method.
-inv_reg_covmat <- function(sigma, lambda){
-	sigma_l = (1-lambda)*sigma + (lambda/4)*diag(length(sigma[1,])) # regularize as suggested
-	return(ginv(sigma_l)) # return inverse of regularized matrix
+## Cross validation
+lambda = seq(0.05, 0.95, 0.05) # initializing lambdas
+p = rep(0.1, 10) # initializing pi
+training_labels = sort(rep(seq(0,9),400)) # readjusting training labels
+errors = list() # initializing list of errors
+for(i in 1:length(lambda)){ # looping through all values of lambda
+	error_i = c() # intialize vector of errors at this value of lambda
+	for (j in 1:5){ # training and testing at this lambda 5 times
+		mu = list() # initializing list of means
+		cv.train.data = list(); cv.test.data = list(); cv.test.labels = list() # initializing cross-validation data lists
+		for (k in 0:9){ # looping through each class
+			inds = sample(500,400) # randomly sample 400
+			cv.train.data[[k+1]] = training.data[which(training.label==k),][inds,]+0 # store training data
+			cv.test.data[[k+1]] = training.data[which(training.label==k),][-inds,]+0 # store test data
+			cv.test.labels[[k+1]] = training.label[which(training.label==k)][-inds] # store test labels
+			mu[[k+1]] = colMeans(cv.train.data[[k+1]]) # calculate mean
+		}
+		cv.train.data.mat = cv.train.data[[1]] # initializing matrix of training data
+		for(k in 2:length(cv.train.data)){ # transforming training data from list to matrix
+			cv.train.data.mat = rbind(cv.train.data.mat, cv.train.data[[k]])
+		}
+		cv.test.data.mat = cv.test.data[[1]] # initializing matrix ot test data
+		for(k in 2:length(cv.test.data)){ # transforming test data from list to matrix
+			cv.test.data.mat = rbind(cv.test.data.mat, cv.test.data[[k]])
+		}
+		sigma = compute_sigma(mu, training_labels, cv.train.data.mat) # computing covariance matrix
+		cv.test.labels = unlist(cv.test.labels) # transforming test labels from list to vector
+		error_i = c(error_i, test_classifier(mu, sigma, p, lambda[i], cv.test.data.mat, cv.test.labels)[[1]]) # appending to vector of errors
+	}
+	errors[[i]] = error_i # appending to list of errors
 }
-
-
-
-
-
-
-## Implementing LDA
-library(MASS)
-library(caret)
-library(lattice)
-library(hda)
-train.lda <- lda(training.data*1, training.label) # Attempting LDA on the training data
-good.indices = c(27:30,32,33,35:36,45:57,64:79,82,84:99,102:120,122:140,142:160,162:180,182:200,202:220,222:240,262:280,282:300,302:319,323:339,343:358,365,378,388) # Listing only non-constant variables
-training.data.clipped <- training.data[,good.indices] # Removing constant variables from data set
-train.lda <- lda(training.data.clipped*1, training.label) # Attempting LDA on clipped training data
-train.cor <- cor(training.data.clipped*1) # Calculating correlation of remaining variables
-train.dissimilarity <- 1 - abs(train.cor) # Creating dissimilarity measure
-distance <- as.dist(train.dissimilarity) # Using dissimilarity as distance measure
-clust <- hclust(distance) # Performing hierarchical clustering on variables
-cluster.vec <- cutree(hc, h=0.10) # Create an index of clusters whose constituents have abs(correlation) > 0.9
-print(cluster.vec)
-training.data.clipped <- training.data.clipped[,-c(278,6)] # Removing collinear variables from training data
-train.lda <- lda(training.data.clipped*1, training.label) # Attempting LDA on modified training data
-test.data.clipped = test.data[,good.indices] # Removing constant variance variables from test
-test.data.clipped = test.data.clipped[,-c(278,6)] # Removing collinear variables from test
-test.prediction = predict(train.lda,newdata=test.data.clipped*1) # Performing prediction on test data
-confusionMatrix(test.prediction$class, test.label)
-pdf("ProjectLDAPlot1.pdf", width=13, height=10)
-par(mar=c(1,1,1,1))
-plot(train.lda, dimen=1, type="both", main="LDA Class Histograms")
+pdf("ErrorPlot.pdf", width=13, height=8)
+plot(sort(rep(lambda,5)), unlist(errors), main="Error Rate vs. Lambda", xlab="Lambda", ylab="Error Rate")
+points(lambda, colMeans(matrix(unlist(errors), nrow=5, byrow=F)), pch=10, cex=5)
 dev.off()
-train.hda <- hda(training.data[1:100,]*1, training.label[1:100], reg.gamm=0.75, reg.lamb=0, noutit=2)
+error_mat = matrix(unlist(errors), nrow=5, byrow=F)
+colnames(error_mat) = lambda
+error_mat
+colMeans(error_mat)
+
+
+## Testing final classifier
+mu = list() # initialize means
+p = rep(0.1,10) # initialize pi
+lambda = 0.2 # set lambda based on cross-validation
+for (k in 0:9){
+	mu[[k+1]] = colMeans(training.data[which(training.label==k),]) # compute mean of k'th digit
+}
+sigma = compute_sigma(mu, training.label, training.data) # compute covariance matrix
+test_results = test_classifier(mu, sigma, p, lambda, test.data, test.label) # test classifier on test data
+test_results[[1]] # print error rate from test
+class_error = c() # initializing class-specific error rate vector
+for (i in 0:9){
+	class_error = c(class_error, 1 - mean((test_results[[2]]==test.label)[test.label==i])) # appending class-specific error rate for i'th class
+}
+class_error # print class-specific error rate vector
+pdf("ClassErrorRate.pdf", width=13, height=8)
+barplot(class_error, main="Class-specific Error Rates", xlab="Class", names.arg=c("0","1","2","3","4","5","6","7","8","9"), ylab="Error Rate")
+dev.off()
+
+
 
